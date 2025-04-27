0b6639277bd4b2da37fbee6fb60e1f6919bd25dd
Deepak Kar <github@deepakk.xyz>
2025-04-27 18:09:18 +0100

    Advent of Code 2024: Day 1 & 2

---
+++ 2024/golang/day01.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
+func part1(left, right []int) int {
+	ans := 0
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	for i := 0; i < len(left); i++ {
+		if left[i] > right[i] {
+			ans += left[i] - right[i]
+		} else {
+			ans += right[i] - left[i]
+		}
+	}
+
+	return ans
+}
+
+func part2(left, right []int) int {
+	ans := 0
+	counter := make(map[int]int)
+
+	for _, value := range right {
+		counter[value]++
+	}
+
+	for _, value := range left {
+		ans += value * counter[value]
+	}
+
+	return ans
+}
+
+func main() {
+	part := os.Args[1]
+	var left []int
+	var right []int
+
+	for {
+		var l, r int
+		_, err := fmt.Scanf("%d %d", &l, &r)
+
+		if err != nil {
+			break
+		}
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	if part == "1" {
+		fmt.Println(part1(left, right))
+	} else {
+		fmt.Println(part2(left, right))
+	}
+}
---
+++ 2024/golang/day02.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func part1(levels []int) bool {
+	previous := -1
+	increasing := true
+	decreasing := true
+
+	for _, value := range levels {
+		if previous != -1 {
+			if value == previous || value > previous+3 || value < previous-3 {
+				return false
+			} else if value > previous {
+				decreasing = false
+			} else {
+				increasing = false
+			}
+		}
+		previous = value
+	}
+
+	return increasing || decreasing
+}
+
+func part2(levels []int) bool {
+	if part1(levels) {
+		return true
+	}
+
+	for skip := 0; skip < len(levels); skip++ {
+		var levelsWithSkip []int
+		for index, value := range levels {
+			if index != skip {
+				levelsWithSkip = append(levelsWithSkip, value)
+			}
+		}
+		if part1(levelsWithSkip) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func main() {
+	part := os.Args[1]
+	safe := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		var levels []int
+		for _, level := range strings.Fields(scanner.Text()) {
+			value, err := strconv.Atoi(level)
+			if err != nil {
+				fmt.Println(err)
+			}
+			levels = append(levels, value)
+		}
+
+		if part == "1" && part1(levels) {
+			safe += 1
+		} else if part2(levels) {
+			safe += 1
+		}
+
+	}
+	fmt.Println(safe)
+}
